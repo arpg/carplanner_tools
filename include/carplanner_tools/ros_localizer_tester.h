a28047f570ac40e89e183bcd669d6f67d622be31
Mike Miles <mikemiles19@gmail.com>
2022-04-12 22:19:23 -0600

    added tester for generating tfs in a circle

---
+++ include/carplanner_tools/ros_localizer_tester.h
@@ -0,0 +1,24 @@
+#include <ros/ros.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <tf/transform_broadcaster.h>
+
+class Tester
+{
+public:
+  Tester(ros::NodeHandle& nh_, ros::NodeHandle& pnh_);
+  ~Tester();
+
+private:
+  void loopFunc(const ros::TimerEvent& event);
+
+private:
+  ros::NodeHandle nh, pnh;
+  ros::Timer loop;
+  std::string parent_frame_id, child_frame_id;
+  float rate;
+  uint iteration;
+  tf::TransformBroadcaster tfcaster;
+};
+
+
+
